aeac1027bb6aed6f4e8a36b5d3147cc6832ae747
Thomas Pope <tpope0928@gmail.com>
2018-09-04 13:37:25 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,19 +1,1 @@
 # Your code goes here!
-class Anagram
-
-  attr_accessor :word
-
-  def initialize(word)
-    @word = word
-  end
-  
-  def match
-    match = []
-    array.each do |word|
-      matches << word if word. split('').sort == self.word.split('').sort 
-    end
-    matches
-  end
-
-
-end
