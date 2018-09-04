2517ba4e33b15e49dc9c5f98a1365217c8c22c51
Thomas Pope <tpope0928@gmail.com>
2018-09-04 13:44:15 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,18 +1,18 @@
 # Your code goes here!
 class Anagram
 
-attr_accessor :word
+  attr_accessor :word
 
-def initialize(word)
-  @word = word
-end
-
-def match(array)
-  matches = []
-  array.each do |word|
-    matches << word if word.split('').sort == self.word.split('').sort
+  def initialize(word)
+    @word = word
   end
-  matches
-end
+
+  def match(array)
+    matches = []
+    array.each do |word|
+      matches << word if word.split('').sort == self.word.split('').sort
+    end
+    matches
+  end
 
 end
